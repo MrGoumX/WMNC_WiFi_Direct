3f742979d4fc11823ec6a6ec5d9da88bee41dbbc
MrGoumX <p3160026@aueb.gr>
2018-10-25 20:39:05 +0300

    Initial commit

---
+++ app/src/main/java/gr/aueb/wmnc/wifidirecttransfer/MainActivity.java
@@ -0,0 +1,55 @@
+package gr.aueb.wmnc.wifidirecttransfer;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final IntentFilter intentFilter = new IntentFilter();
+    private WifiP2pManager.Channel mChannel;
+    private WifiP2pManager mManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.refresh:
+                //TODO TRIGGER WIFI DIRECT DISCOVERY
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /*@Override
+    public void onReceive(Context context, Intent intent){
+        String action = intent.getAction();
+
+    }*/
+}
